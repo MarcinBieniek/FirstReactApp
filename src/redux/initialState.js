c7baab8603683e39e5ce5495695568a4701b8c95
Marcin Bieniek <marcingim@gmail.com>
2022-07-08 21:31:06 +0200

    test

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -17,6 +17,7 @@ const initialState = {
       }
     ],
   
+    
     cards: [
       { id: 1, columnId: 1, title: 'This is Going to Hurt' },
       { id: 2, columnId: 1, title: 'Interpreter of Maladies' },

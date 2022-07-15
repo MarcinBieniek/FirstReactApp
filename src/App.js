73b541962954653bc0a990969a578c67be3c9cf5
Marcin Bieniek <marcingim@gmail.com>
2022-07-15 12:10:24 +0200

    Created separate Home component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
-import Hero from './components/Hero/Hero'
-import SearchForm from './components/SearchForm/SearchForm'
-import List from './components/List/List'
 import Container from './components/Container/Container'
 import NavBar from './components/NavBar/NavBar'
 import About from './components/About/About'
 import Favourite from './components/Favourite/Favourite'
+import Home from './components/Home/Home'
 
 const App = () => {
   return (
     <main>  
       <NavBar />
       <Container>
-        <Hero />
-        <SearchForm />
-        <List />
+        <Home />
         <About />
         <Favourite />
       </Container>
